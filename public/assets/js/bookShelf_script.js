201c9ca88ea1e738e2a41dd3ca26ea8002f7aa7d
Samantha Capps <samantha@Cheez.local>
2018-05-01 16:46:02 -0400

    mySQL

--- public/assets/js/bookShelf_script.js
+++ public/assets/js/bookShelf_script.js
@@ -1,8 +1,18 @@
+function loopForListByTitle(results) {
+    var end = results.length;
+    for (let i = 0; i >= end; i++) {
+        $(".books").append(results[i].title + " by " + results[i].author);
+        if (i < end - 1) {
+            $(".books").append("<br><br>");
+        }
+    }
+};
+
 $("#title").click(function() {
     $.ajax({
         method: "GET",
         url: "/listByTitle"
     }).done(function(results) {
-        $(".books").text(results[0].title);
+        loopForListByTitle(results);
     });
 });
